c5f14a2214c44493dda3bc5acdad1a2c5c38bb33
mrtan <freemrtan@gmail.com>
2019-12-17 18:27:28 +0800

    add remove shell

---
+++ server/remove.sh
@@ -0,0 +1,1 @@
+docker rm -f transLangApi && docker rmi -f test.tapplock.com:4000/transLang:test
--- server/start.sh
+++ server/start.sh
@@ -1,1 +1,1 @@
- docker run -d -i -t -p 4000:4000 test.tapplock.com:4000/transLang:test
+ docker run -d -i -t --name transLangApi -p 4000:4000 test.tapplock.com:4000/transLang:test
